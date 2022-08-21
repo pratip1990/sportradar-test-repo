9c1f8e846e00d0b12b5b649db72897a77bbdeddb
pratip1990 <pratip.halder@gmail.com>
2022-08-21 18:29:47 +0530

    implemented basic validation for create and update Game

--- scoreboard-lib/src/main/java/com/ph/scoreboardlib/core/game/util/GameUtil.java
+++ scoreboard-lib/src/main/java/com/ph/scoreboardlib/core/game/util/GameUtil.java
@@ -6,6 +6,7 @@ package com.ph.scoreboardlib.core.game.util;
 import java.util.TreeSet;
 
 import com.ph.scoreboardlib.core.game.Game;
+import com.ph.scoreboardlib.core.game.GameStatus;
 
 /**
  * @author Pratip
@@ -14,13 +15,28 @@ import com.ph.scoreboardlib.core.game.Game;
 public class GameUtil {
 
 	public static boolean validateGameForCreate(TreeSet<Game> scores, Game game) {
-		// TODO Auto-generated method stub
-		return false;
+		if(null == game) {
+			System.out.println("Invalid Game: Game is not instantiated");
+			return false;
+		} else if (scores.contains(game)) {
+			System.out.println("Invalid Game: Game is already started");
+			return false;
+		}
+		return true;
 	}
 
 	public static boolean validateGameForUpdate(TreeSet<Game> scores, Game game) {
-		// TODO Auto-generated method stub
-		return false;
+		if(null == game) {
+			System.out.println("Invalid Game: Game is not instantiated");
+			return false;
+		} else if (!scores.contains(game)) {
+			System.out.println("Invalid Game: Game is not started yet");
+			return false;
+		} else if (GameStatus.FINISH == game.getStatus()){
+			System.out.println("Invalid Game: FINISHED Game can not be updated");
+			return false;
+		}
+		return true;
 	}
 
 }
